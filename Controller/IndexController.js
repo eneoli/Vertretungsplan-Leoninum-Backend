418a70756a8117f4aaf0d018d9ee0187d8fe5050
Oliver Enes <oliver@enesnet.de>
2018-12-30 18:31:46 +0100

    " removed

--- Controller/IndexController.js
+++ Controller/IndexController.js
@@ -1,4 +1,4 @@
 exports.onIndex = function (req, res)
 {
-    res.send("\"<h2 style='height: 2%'>Live aus dem Serverraum</h2><a href='https://i.imgflip.com/17q7vm.jpg'><img style='display:block; object-fit: contain;width: 100%; height: 98%' src='https://i.imgflip.com/17q7vm.jpg'/></a><br><a href='https://imgflip.com/gif-maker'> via Imgflip GIF Maker</a>");
+    res.send("<h2 style='height: 2%'>Live aus dem Serverraum</h2><a href='https://i.imgflip.com/17q7vm.jpg'><img style='display:block; object-fit: contain;width: 100%; height: 98%' src='https://i.imgflip.com/17q7vm.jpg'/></a><br><a href='https://imgflip.com/gif-maker'> via Imgflip GIF Maker</a>");
 };
