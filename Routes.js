1a5637a28f2e4f832a4034ff4c26262df4342e39
Oliver Enes <oliver@enesnet.de>
2018-12-30 02:20:57 +0100

    RSA Verschlüsselung hinzugefügt --> Autologin

--- Routes.js
+++ Routes.js
@@ -3,9 +3,12 @@ module.exports = function (app)
     var IndexController = require("./Controller/IndexController");
     var MoodleSessionController = require("./Controller/MoodleSessionController");
     var DayController = require("./Controller/DayController");
+    var SecurityController = require("./Controller/Security/SecurityController");
 
     app.get("/moodleSession", MoodleSessionController.onMoodleSession);
     app.get("/fetch/:day",DayController.onFetchDay);
     app.get("/", IndexController.onIndex);
     app.get("/validateSession", MoodleSessionController.validateMoodleSession);
+    app.post("/encryptCredentials", SecurityController.encryptCredentials);
+    app.post("/decryptCredentials", SecurityController.decryptCredentials);
 };
